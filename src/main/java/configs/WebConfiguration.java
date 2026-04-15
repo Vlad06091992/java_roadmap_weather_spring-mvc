aa7911d27edeeb6e764989cef215c8291795cd8d
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2026-04-15 21:21:32 +0500

    add general spring mvc config

---
+++ src/main/java/configs/WebConfiguration.java
@@ -0,0 +1,18 @@
+package configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+
+    // Implement configuration methods...
+}
