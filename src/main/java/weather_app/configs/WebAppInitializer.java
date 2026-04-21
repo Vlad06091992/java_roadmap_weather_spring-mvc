194d92bcf019a3b9d88e2301825aff5b0912d7f4
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2026-04-21 09:16:44 +0500

    configured Liquibase for migrations and wrote a migration file

--- src/main/java/weather_app/configs/WebAppInitializer.java
+++ src/main/java/weather_app/configs/WebAppInitializer.java
@@ -9,7 +9,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     }
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{WebConfiguration.class, ThymeleafConfig.class, HibernateConf.class};
+        return new Class<?>[]{WebConfiguration.class, ThymeleafConfig.class, HibernateConf.class, LiquibaseConfig.class};
     }
     @Override
     protected String[] getServletMappings() {
