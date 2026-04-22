7fbb3ba51f167eb84c6bdc7f35e4c9247ef26a84
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2026-04-22 10:08:49 +0500

    added logger for project

--- src/main/java/weather_app/controllers/Registration.java
+++ src/main/java/weather_app/controllers/Registration.java
@@ -2,6 +2,9 @@ package weather_app.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +20,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class Registration {
     private final ViewErrorHandler viewErrorHandler;
     private final UserService userService ;
@@ -25,9 +29,11 @@ public class Registration {
     public String register(@ModelAttribute @Valid RegisterDTO registerDTO, BindingResult bindingResult, Model model) {
         List<FieldError> errorList = bindingResult.getFieldErrors();
         if (bindingResult.hasErrors()) {
+            log.info("Validation errors found {}", bindingResult.getFieldErrors());
             viewErrorHandler.addErrorsToModel(errorList, model);
             return "register";
         } else {
+            log.info("user succefully created {}", registerDTO);
             //создать юзера, куда-то редиректунть
             userService.addUser(registerDTO.getUsername(), registerDTO.getPassword());
             System.out.println(errorList);
